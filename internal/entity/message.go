bf354de2588f5e63cb35d8dc83d285233fe5648c
Uriel Robles <urielrobles802@gmail.com>
2023-05-16 22:31:09 -0600

    message entity

---
+++ internal/entity/message.go
@@ -0,0 +1,8 @@
+package models
+
+type Message struct {
+	Content string `json:"content,omitempty"`
+	Channel string `json:"channel,omitempty"`
+	Command int    `json:"command,omitempty"`
+	Err     string `json:"err,omitempty"`
+}
