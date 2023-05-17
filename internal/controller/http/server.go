f3a1bd11b6610a55ba7ab2fc3cac36d6438de153
Uriel Robles <urielrobles802@gmail.com>
2023-05-16 22:32:13 -0600

    messages endpoint and jwt middleware

--- internal/controller/http/server.go
+++ internal/controller/http/server.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v7"
 	"github.com/roblesoft/topics/internal/usecase"
+	"github.com/roblesoft/topics/pkg/token"
 )
 
 type Server struct {
@@ -25,22 +28,36 @@ func (server *Server) Router() *gin.Engine {
 
 func (server *Server) setupRouter() {
 	var (
-		router = gin.Default()
-		api    = router.Group("/api")
-		v1     = api.Group("/v1")
-		users  = v1.Group("/users")
-		chat   = v1.Group("/chat")
+		router   = gin.Default()
+		api      = router.Group("/api")
+		v1       = api.Group("/v1")
+		users    = v1.Group("/users")
+		messages = users.Group("/:user/messages")
 	)
-
-	users.POST("/register", server.Register)
-	users.POST("/login", server.Login)
-	chat.GET("/index", server.ChatnetHandler)
+	api.Use(gin.Logger())
 
 	v1.GET("/healthcheck/", server.HealthCheck)
+	users.POST("/register", server.Register)
+	users.POST("/login", server.Login)
+
+	messages.Use(JwtAuthMiddleware())
+	messages.GET("/", server.ChatnetHandler)
 
 	server.router = router
 }
 
 func (r *Server) Start() {
 	r.router.Run(r.Port)
+}
+
+func JwtAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := token.TokenValid(c)
+		if err != nil {
+			c.String(http.StatusUnauthorized, "Unauthorized")
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
 }
